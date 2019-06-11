ded58a08756dbab0c590346bc8118325469ae457
Herman <hermanborrego@gmail.com>
2019-06-11 16:59:50 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // wait until window is loaded - meaning all images, stylesheets, js, fonts, media assets, and links
+  window.addEventListener(
+    "load",
+    function() {
+      document.body.style.display = "block";
+
+      var tl = new TimelineMax();
+
+      tl.staggerFrom(
+        "h1",
+        2,
+        {
+          opacity: 0,
+          y: -40,
+          ease: Power2.easeInOut
+        },
+        0.2
+      );
+    },
+    false
+  );
+});
