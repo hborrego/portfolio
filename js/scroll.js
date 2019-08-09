15812efc816b832d36217a982f6c3e943156e2cc
Herman <hermanborrego@gmail.com>
2019-08-09 10:09:47 -0700

    added 229, bloor dufferin and thorncliffe

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,7 @@ $("a").on("click", function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault();
+      // event.preventDefault();
   
       // Store hash
       var hash = this.hash;
---
+++ js/thorncliffe.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // wait until window is loaded - meaning all images, stylesheets, js, fonts, media assets, and links
+    window.addEventListener(
+      "load",
+      function() {
+        document.body.style.display = "block";
+    },
+    false
+  );
+});
